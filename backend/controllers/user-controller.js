7d1eff2f63829d48e67ffd345cc941db167f7ba6
AmanJha1105 <amanjha1105@gmail.com>
2024-02-22 17:04:25 +0530

    Add Login route

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -36,4 +36,36 @@ const signup= async(req,res,next)=>{
     return res.status(201).json({message:user});
 }
 
+//creating login route
+
+const login =async(req,res,next)=>{
+
+    const {email,password}=req.body;
+
+    //check if user exists
+    let existingUser;
+    try {
+        existingUser= await User.findOne({email:email});
+    } catch (error) {
+        console.log("something went wrong",error);
+    }
+    //if user does not exist throw error
+    if(!existingUser){
+        return res.staus(400).json({message:"Invalid Credentials"});
+    }
+
+    //check password
+
+    const isPasswordCorrect= bcrypt.compareSync(password,existingUser.password);
+    
+    if(!isPasswordCorrect){
+        return res.status(400).json({message:"Invalid details"});
+    }
+    
+    //if password correct then login
+
+    return res.status(200).json({message:"Successfully logged in"});
+}
+
 exports.signup=signup;
+exports.login=login;
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,7 +1,7 @@
 const express= require('express');
-const { signup } = require('../controllers/user-controller');
+const { signup, login } = require('../controllers/user-controller');
 const router= express.Router();
 
 router.post("/signup",signup);
-
+router.post("/login",login);
 module.exports=router;
